78b3ff8ef3f176072e43685fad5d9d2793a3f368
Christian Lopes <christianlopes@dhcp-v077-052.mobile.uci.edu>
2018-05-23 21:31:41 -0700

    adding mongo code

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = 3000 || process.env.PORT
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 // Initialize Express
 var app = express();
